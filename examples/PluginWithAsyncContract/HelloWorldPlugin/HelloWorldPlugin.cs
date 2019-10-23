b074e428adcf5cc3c991b2aafad6f0d50728b11c
Maarten Merken <merken.maarten@gmail.com>
2019-10-23 11:41:52 +0200

    Added example for Plugin with Async contract (return type = Task)

---
+++ examples/PluginWithAsyncContract/HelloWorldPlugin/HelloWorldPlugin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Contract;
+using Newtonsoft.Json;
+using Prise.Infrastructure;
+
+namespace HelloWorldPlugin
+{
+    [Plugin(PluginType = typeof(IHelloWorldPlugin))]
+    public class HelloWorldPlugin : IHelloWorldPlugin
+    {
+        public async Task<string> SayHelloAsync(string language, string input)
+        {
+            var dictionary = await GetDictionary();
+            if (dictionary.ContainsKey(language.ToLower()))
+                return $"{dictionary[language.ToLower()]} {input}";
+            throw new NotSupportedException($"Language {language} not found in dictionary");
+        }
+
+        public async Task<HelloDictionary> GetHelloDictionaryAsync()
+        {
+            var dictionary = await GetDictionary();
+            return new HelloDictionary(dictionary);
+        }
+
+        private async Task<Dictionary<string, string>> GetDictionary()
+        {
+            using (var stream = new StreamReader("Plugins\\Dictionary.json"))
+            {
+                var json = await stream.ReadToEndAsync();
+                return JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+            }
+        }
+    }
+}
---
+++ examples/PluginWithAsyncContract/MyHost/Controllers/HelloController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using Contract;
+using Prise.Infrastructure.NetCore;
+
+namespace MyHost.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        private readonly IHelloWorldPlugin _helloWorldPlugin;
+
+        public HelloController(IHelloWorldPlugin helloWorldPlugin)
+        {
+            _helloWorldPlugin = helloWorldPlugin;
+        }
+
+        [HttpGet]
+        public async Task<string> Get([FromQuery]string language, [FromQuery]string input)
+        {
+            try
+            {
+                var result = await _helloWorldPlugin.SayHelloAsync(language, input);
+
+                return result;
+            }
+            catch (AggregateException ex)
+            {
+                // u-oh, is the language not supported ?
+            }
+
+            var supportedLanguages = await _helloWorldPlugin.GetHelloDictionaryAsync();
+            return $"Language {language} is not supported. Supported languages are : {String.Join(',', supportedLanguages.SupportedLanugages)}";
+        }
+    }
+}
