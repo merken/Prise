ed45eaf3a3145016255b2481da9f64751f01a1bf
Maarten MERKEN <maarten.merken@aldautomotive.com>
2019-10-25 13:10:53 +0200

    Added sad test

---
+++ examples/ComplexPlugin/Tests/SadPathTests.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AppHost.Models;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace Tests
+{
+    public class SadPathTests : PluginTestBase,
+         IClassFixture<AppHostWebApplicationFactory>
+    {
+        private readonly HttpClient _client;
+        private readonly AppHostWebApplicationFactory _factory;
+
+        public SadPathTests(
+                 AppHostWebApplicationFactory factory)
+        {
+            _factory = factory;
+            _client = factory.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false
+            });
+        }
+
+        [Fact]
+        public async Task PluginD_DoesNotExists()
+        {
+            // Arrange
+            var payload = new CalculationRequestModel
+            {
+                A = 100,
+                B = 150
+            };
+
+            //Act
+            await Assert.ThrowsAsync<System.IO.FileNotFoundException>(async () => await Post<CalculationResponseModel>(_client, "PluginD", "/eager", payload));
+        }
+    }
+}
