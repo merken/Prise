f96127a7325293b58f0c9fbe2d8ae448d3c5059b
Maarten <merken.maarten@gmail.com>
2019-10-30 08:51:35 +0100

    Use BaseDirectory for rootpath

--- examples/AvaloniaAppWithPlugin/AppHost/Program.cs
+++ examples/AvaloniaAppWithPlugin/AppHost/Program.cs
@@ -41,10 +41,7 @@ namespace AppHost
 
         private static string GetRootPath()
         {
-            string codeBase = typeof(App).Assembly.Location;
-            UriBuilder uri = new UriBuilder(codeBase);
-            string path = Uri.UnescapeDataString(uri.Path);
-            return Path.GetDirectoryName(path);
+            return AppDomain.CurrentDomain.BaseDirectory;
         }
     }
 }
