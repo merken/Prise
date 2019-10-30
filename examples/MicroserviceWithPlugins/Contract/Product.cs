c5033567cc9a942b2b41bc828f1f5ae23b9dd99c
Maarten MERKEN <maarten.merken@aldautomotive.com>
2019-10-30 13:53:54 +0100

    Work in progress

---
+++ examples/MicroserviceWithPlugins/Contract/Product.cs
@@ -0,0 +1,11 @@
+namespace Contract
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SKU { get; set; }
+        public string Description { get; set; }
+        public decimal PriceExlVAT { get; set; }
+    }
+}
---
+++ examples/MicroserviceWithPlugins/CosmosDbPlugin/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CosmosDbPlugin
+{
+    public class Class1
+    {
+    }
+}
