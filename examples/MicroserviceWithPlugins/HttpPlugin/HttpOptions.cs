e155fbf7185f6019fe23dafeda4a48f4576250b2
Maarten MERKEN <maarten.merken@aldautomotive.com>
2019-11-21 09:05:51 +0100

    Remove Mongo in favor of HTTPPlugin

---
+++ examples/MicroserviceWithPlugins/HttpPlugin/HttpOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HttpPlugin
+{
+    public class HttpConfig
+    {
+        public string BaseUrl { get; set; }
+    }
+}
