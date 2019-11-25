9719bf9bfe0d2c495d546cdb783c385bfd9777f7
Maarten Merken <merken.maarten@gmail.com>
2019-11-25 15:54:10 +0100

    WIP

---
+++ examples/AssemblyDiscovery/Plugins/PluginTester/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.FileExtensions;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.DependencyInjection;
+using ProductsReaderPlugin;
+using System;
+
+namespace PluginTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+              .AddJsonFile("appsettings.json", true, true)
+              .Build();
+            var bootstrapper = new TableStorageProductsReaderBootstrapper();
+
+            var services = new ServiceCollection();
+            services.AddSingleton<IConfiguration>(config);
+            var serviceProvider = bootstrapper.Bootstrap(services).BuildServiceProvider();
+            var reader = TableStorageProductsReader.ThisIsTheFactoryMethod(serviceProvider);
+
+            Console.WriteLine(reader.All().Result);
+            Console.Read();
+        }
+    }
+}
