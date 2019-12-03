966672b70a3acce5bc325104f404159df72b45b2
Maarten <merken.maarten@gmail.com>
2019-12-03 22:02:19 +0100

    WIP

--- examples/MultiTenantMicroservice/Products.API/Startup.cs
+++ examples/MultiTenantMicroservice/Products.API/Startup.cs
@@ -52,7 +52,7 @@ namespace Products.API
                     // this way, the plugins can read their own config section from the appsettings.json
                     sharedServices.AddSingleton(Configuration);
                })
-               .WithHostType<BinderOptions>()
+               //.WithHostType<BinderOptions>()
            );
         }
 
