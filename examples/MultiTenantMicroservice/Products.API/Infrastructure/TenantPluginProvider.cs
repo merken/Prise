21a6407b5d6bc84a34df8bf2fdf26f00798cd8c7
Maarten MERKEN <maarten.merken@aldautomotive.com>
2019-12-04 08:19:01 +0100

    Fixed tests

--- examples/MultiTenantMicroservice/Products.API/Infrastructure/TenantPluginProvider.cs
+++ examples/MultiTenantMicroservice/Products.API/Infrastructure/TenantPluginProvider.cs
@@ -27,7 +27,7 @@ namespace Products.API.Infrastructure
         private string GetPluginFromContext()
         {
             if (!this.contextAccessor.HttpContext.Request.Headers["Tenant"].Any())
-                return "OldSQLPlugin"; //The old plugin is a netcoreapp2.1 plugin, it should work on both MyHost and MyHost2
+                return "SQLPlugin"; //The old plugin is a netcoreapp2.1 plugin, it should work on both MyHost and MyHost2
 
             var tenant = this.contextAccessor.HttpContext.Request.Headers["Tenant"].First();
             var configPair = this.tenantConfig.Configuration
