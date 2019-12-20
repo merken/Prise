40a08c09e50240105aeba10fc1043bb87369e513
Maarten Merken <merken.maarten@gmail.com>
2019-12-20 10:55:06 +0100

    Exception must be serializable

--- src/Prise/Exceptions/PrisePluginException.cs
+++ src/Prise/Exceptions/PrisePluginException.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace Prise
 {
+    [Serializable]
     public class PrisePluginException : Exception
     {
         public PrisePluginException(string message) : base(message)
@@ -9,6 +11,14 @@ namespace Prise
         }
 
         public PrisePluginException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public PrisePluginException()
+        {
+        }
+
+        protected PrisePluginException(SerializationInfo info, StreamingContext context) : base(info, context)
         {
         }
     }
