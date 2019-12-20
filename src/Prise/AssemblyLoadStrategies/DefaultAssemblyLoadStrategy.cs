bc5741c4631ad8e2f300e0357518a2214cbc3f9c
Maarten MERKEN <maarten.merken@aldautomotive.com>
2019-12-20 14:11:15 +0100

    Comments

--- src/Prise/AssemblyLoadStrategies/DefaultAssemblyLoadStrategy.cs
+++ src/Prise/AssemblyLoadStrategies/DefaultAssemblyLoadStrategy.cs
@@ -2,15 +2,21 @@
 using System.Linq;
 using System.Reflection;
 using Prise.Infrastructure;
+using Prise.Util;
 
 namespace Prise
 {
     /// <summary>
     /// This is the default assembly load strategy for any requested assembly.
-    /// Firstly, the assembly is loaded from the App Domain, keeping into account that a requested assembly is a host assemly and not explicitly setup to be loaded from the remote.
-    /// Secondly, the assembly is loaded from the plugin dependency context. If the plugin requires this assembly based on the deps.json file.
-    /// Thirdly, the assembly is loaded from the remote location
-    /// The loading strategy is a pipeline, if the return value is allowed to proceed, it will continue, otherwise it will exit the pipe.
+    /// For Managed Assemblies, this is the pipeline:
+    ///     Firstly, the assembly is loaded from the App Domain, keeping into account that a requested assembly is a host assemly and not explicitly setup to be loaded from the remote.
+    ///     Secondly, the assembly is loaded from the plugin dependency context. If the plugin requires this assembly based on the deps.json file.
+    ///     Thirdly, the assembly is loaded from the remote location
+    ///     The loading strategy is a pipeline, if the return value is allowed to proceed, it will continue, otherwise it will exit the pipe.
+    /// For Unmanaged Assemblies, this is the pipeline:
+    ///     Firstly, Load the assembly from the plugin dependency context
+    ///     Secondly, load the assembly from the host app domain
+    ///     Thirdly, load the assembly from the plugin remote location
     /// </summary>
     public class DefaultAssemblyLoadStrategy : IAssemblyLoadStrategy
     {
@@ -19,8 +25,8 @@ namespace Prise
 
         public DefaultAssemblyLoadStrategy(IPluginLoadContext pluginLoadContext, IPluginDependencyContext pluginDependencyContext)
         {
-            this.pluginLoadContext = pluginLoadContext;
-            this.pluginDependencyContext = pluginDependencyContext;
+            this.pluginLoadContext = pluginLoadContext.ThrowIfNull(nameof(pluginLoadContext));
+            this.pluginDependencyContext = pluginDependencyContext.ThrowIfNull(nameof(pluginDependencyContext));
         }
 
         public virtual Assembly LoadAssembly(AssemblyName assemblyName,
