5521b17cd1cad56d1ee57c6d006075989875fb8d
Maarten MERKEN <maarten.merken@aldautomotive.com>
2019-12-20 14:11:40 +0100

    Validation util

---
+++ src/Prise/Util/Validation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Prise.Util
+{
+    public static class ValidationExtensions
+    {
+        public static T ThrowIfNull<T>(this T obj, string member)
+        {
+            if (obj == null)
+                throw new ArgumentNullException(member);
+            return obj;
+        }
+
+        public static string ThrowIfNullOrEmpty(this string obj, string member)
+        {
+            if (String.IsNullOrEmpty(obj))
+                throw new ArgumentNullException(member);
+            return obj;
+        }
+
+        public static string ThrowIfNullOrWhiteSpace(this string obj, string member)
+        {
+            if (String.IsNullOrWhiteSpace(obj))
+                throw new ArgumentNullException(member);
+            return obj;
+        }
+    }
+}
