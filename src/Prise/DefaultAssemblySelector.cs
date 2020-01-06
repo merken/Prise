e97ef86fc092f5ed8a49973611f54a2b7a764518
Maarten <merken.maarten@gmail.com>
2020-01-06 04:58:20 +0100

    WIP

---
+++ src/Prise/DefaultAssemblySelector.cs
@@ -0,0 +1,24 @@
+﻿using Prise.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prise
+{
+    public class DefaultAssemblySelector<T> : IAssemblySelector<T>
+    {
+        private readonly Func<IEnumerable<Type>, IEnumerable<Type>> pluginSelector;
+
+        public DefaultPluginSelector(Func<IEnumerable<Type>, IEnumerable<Type>> pluginSelector = null)
+        {
+            this.pluginSelector = pluginSelector;
+        }
+
+        public IEnumerable<Type> SelectPlugins(IEnumerable<Type> pluginTypes)
+        {
+            if (pluginSelector == null)
+                return pluginTypes;
+            return pluginSelector.Invoke(pluginTypes);
+        }
+    }
+}
---
+++ src/Prise/Infrastructure/IAssemblySelector.cs
@@ -0,0 +1,16 @@
+﻿using Prise.AssemblyScanning;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prise.Infrastructure
+{
+    /// <summary>
+    /// An AssemblySelector selects the assemblies to load based on the types that were found after scanning the assembly
+    /// </summary>
+    public interface IAssemblySelector<T>
+    {
+        Task<IEnumerable<AssemblyScanResult<T>>> SelectAssemblies(IEnumerable<AssemblyScanResult<T>>);
+    }
+}
