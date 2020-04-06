7715027f98ec61f9fa6404d6d16fbb484d318b7a
Maarten Merken <merken.maarten@gmail.com>
2020-04-06 20:24:40 +0200

    Code cleanup

--- src/Prise/DefaultPluginServiceProvider.cs
+++ src/Prise/DefaultPluginServiceProvider.cs
@@ -20,19 +20,29 @@ namespace Prise
 
         public object GetPluginService(Type type)
         {
+            // Plugin services are registered via a PluginBootstrapper, eventually they'll land inside the localProvider.
+            var sharedType = this.sharedTypes.FirstOrDefault(t => t.Name == type.Name);
+            if (sharedType == null)
+                throw new PrisePluginException($"An instance of type {type.Name} is required to activate this plugin, but it was not registered as a Plugin Type, please provide this service via a PluginBootstrapper builder.");
+
             var instance = this.localProvider.GetService(type);
+            if (instance == null)
+                throw new PrisePluginException($"An instance of Plugin Service type {type.Name} could not be properly constructed (null).");
 
             return instance;
         }
 
         public object GetHostService(Type type)
         {
+            // Host Services can either be provided as Shared Service or Host Service
             var hostType = this.hostTypes.FirstOrDefault(t => t.Name == type.Name);
             var sharedType = this.sharedTypes.FirstOrDefault(t => t.Name == type.Name);
-            //if (hostType == null)
-            //    throw new PrisePluginException($"An instance of type {type.Name} is required to activate this plugin, but it was not registered as a Shared Type, please configure this type in the ConfigureSharedServices builder.");
+            if ((hostType ?? sharedType) == null)
+                throw new PrisePluginException($"An instance of type {type.Name} is required to activate this plugin, but it was not registered as a Shared Type or a Host Type, please configure this type via the UseHostServices, ConfigureHostServices or ConfigureSharedServices builder method.");
 
             var instance = this.localProvider.GetService(hostType ?? sharedType);
+            if (instance == null)
+                throw new PrisePluginException($"An instance of Host Service type {type.Name} could not be properly constructed (null).");
 
             return instance;
         }
