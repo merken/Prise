d93b757736c6e7dfa62b3573c1cb7469cfe56023
Maarten Merken <merken.maarten@gmail.com>
2020-09-19 10:48:03 +0200

    WIP

--- src/v2/Prise.Console2/Program.cs
+++ src/v2/Prise.Console2/Program.cs
@@ -1,25 +1,70 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using System.Reflection;
-using Prise.Plugin.Sql.Legacy;
+using System.Runtime.Loader;
+using System.Threading.Tasks;
+using Prise.Console.Contract;
 using Prise.Web;
-
 namespace Prise.Console2
 {
+    
+    class LoadContext : AssemblyLoadContext
+    {
+
+        internal static async Task<Stream> LoadFileFromLocalDisk(string fullPathToAssembly)
+        {
+            var memoryStream = new MemoryStream();
+            using (var stream = new FileStream(fullPathToAssembly, FileMode.Open, FileAccess.Read))
+            {
+                memoryStream.SetLength(stream.Length);
+                await stream.ReadAsync(memoryStream.GetBuffer(), 0, (int)stream.Length);
+            }
+            return memoryStream;
+        }
+
+        public async Task<Assembly> LoadPLugin(string path)
+        {
+            return base.LoadFromStream(await LoadFileFromLocalDisk(path));
+        }
+        protected override Assembly Load(AssemblyName assemblyName)
+        {
+            var assembly = Assembly.Load(assemblyName);
+            System.Console.WriteLine($"Loaded {assembly.FullName} from {assembly.Location}");
+            return assembly;
+        }
+
+        protected override IntPtr LoadUnmanagedDll(string unmanagedDllName)
+        {
+            return base.LoadUnmanagedDll(unmanagedDllName);
+        }
+    }
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            var sqlPlugin = new SqlStoragePlugin();
-            typeof(SqlStoragePlugin).GetTypeInfo()
+
+            var context = new LoadContext();
+            var pathToThisProgram = Assembly.GetExecutingAssembly() // this assembly location (/bin/Debug/netcoreapp3.1)
+                                                  .Location;
+            var pathToExecutingDir = Path.GetDirectoryName(pathToThisProgram);
+            var pathToPlugin = Path.GetFullPath(Path.Combine(pathToExecutingDir, "../../../../Packages/Prise.Plugin.Sql.Legacy/bin/Debug/netcoreapp2.1"));
+
+            var pluginAssembly = Path.Combine(pathToPlugin, "Prise.Plugin.Sql.Legacy.dll");
+            var assembly = await context.LoadPLugin(pluginAssembly);
+            var type = assembly.GetType("Prise.Plugin.Sql.Legacy.SqlStoragePlugin");
+            var instance = assembly.CreateInstance(type.FullName, true);
+            instance.GetType().GetTypeInfo()
                             .DeclaredFields
                                 .First(f => f.Name == "configurationService")
-                                .SetValue(sqlPlugin, new AppSettingsConfigurationService());
+                                .SetValue(instance, new AppSettingsConfigurationService());
 
-            sqlPlugin.OnActivated();
+            instance.GetType().GetTypeInfo().GetMethod("OnActivated").Invoke(instance, null);
+
+            var plugin = instance as IStoragePlugin;
 
             // TODO THIS WORKS BUT PLATFORM UNSUPPORTED ON NET CORE 2????
-            System.Console.WriteLine(sqlPlugin.GetData(new Console.Contract.PluginObject
+            System.Console.WriteLine(plugin.GetData(new Console.Contract.PluginObject
             {
                 Text = "999"
             }).Result.Text);
