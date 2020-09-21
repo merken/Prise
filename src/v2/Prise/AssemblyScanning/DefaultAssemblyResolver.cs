f01960dc957d7931a84902c33d6a48f11d0aaf00
Maarten Merken <merken.maarten@gmail.com>
2020-09-21 15:42:47 +0200

    Show correct error when loading a newer plugin runtime on an older host runtime

--- src/v2/Prise/AssemblyScanning/DefaultAssemblyResolver.cs
+++ src/v2/Prise/AssemblyScanning/DefaultAssemblyResolver.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -19,7 +20,17 @@ namespace Prise.AssemblyScanning
         {
             // We know these assemblies are located in the Host, so don't bother loading them from the plugin location
             if (this.platformAssemblies.Contains(assemblyName.Name))
-                return context.LoadFromAssemblyPath(AssemblyLoadContext.Default.LoadFromAssemblyName(assemblyName).Location);
+            {
+                try
+                {
+                    return context.LoadFromAssemblyPath(AssemblyLoadContext.Default.LoadFromAssemblyName(assemblyName).Location);
+                }
+                catch (FileNotFoundException) when (assemblyName?.Name == "System.Runtime")
+                {
+                    var hostRuntimeAssembly = typeof(System.Runtime.GCSettings).GetTypeInfo().Assembly;
+                    throw new AssemblyScanningException($"System.Runtime {assemblyName.Version} failed to load. Are you trying to load a new plugin into an old host? Host Runtime Version: {hostRuntimeAssembly.GetName().Version} on {hostRuntimeAssembly.CodeBase}");
+                }
+            }
 
             // Check if the file is found in the plugin location
             var candidateFile = Path.Combine(assemblyPath, $"{assemblyName.Name}.dll");
