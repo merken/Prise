eb2940868634708d6c76e83f83655515415f84f3
Maarten Merken <merken.maarten@gmail.com>
2020-09-21 12:56:08 +0200

    Use Version

--- src/v2/Prise/AssemblyLoading/PluginDependency.cs
+++ src/v2/Prise/AssemblyLoading/PluginDependency.cs
@@ -1,9 +1,11 @@
+using System;
+
 namespace Prise.AssemblyLoading
 {
     public class PluginDependency
     {
         public string DependencyNameWithoutExtension { get; set; }
-        public string Version { get; set; }
+        public Version Version { get; set; }
         public string DependencyPath { get; set; }
         public string ProbingPath { get; set; }
     }
