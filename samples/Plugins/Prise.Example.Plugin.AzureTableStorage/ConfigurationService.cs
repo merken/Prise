d4beb5ea5a7c4c6b2afd7674e78f292607cd39fd
Maarten Merken <merken.maarten@gmail.com>
2020-09-22 14:55:58 +0200

    Added Plugins dir

---
+++ samples/Plugins/Prise.Example.Plugin.AzureTableStorage/ConfigurationService.cs
@@ -0,0 +1,15 @@
+
+using Prise.Example.Contract;
+
+namespace Prise.Example.Plugin.AzureTableStorage
+{
+    public class ConfigurationService : Prise.PluginBridge.PluginBridge, IConfigurationService
+    {
+        public ConfigurationService(object hostService) : base(hostService) { }
+
+        public string GetConfigurationValueForKey(string key)
+        {
+            return this.InvokeThisMethodOnHostService<string>(new[] { key });
+        }
+    }
+}
