d53e5776d7bac2e27d69bd34101c4a8d8fab1083
Maarten Merken <merken.maarten@gmail.com>
2020-10-02 13:26:58 +0200

    Fix Twitter Mvc plugin

--- samples/Plugins/MvcPlugin.Twitter/TwitterWidgetMVCPlugin.cs
+++ samples/Plugins/MvcPlugin.Twitter/TwitterWidgetMVCPlugin.cs
@@ -3,9 +3,9 @@ using Example.Contract;
 
 namespace TwitterWidgetPlugin
 {
-    [Plugin(PluginType = typeof(IMVCPlugin))]
-    [MVCFeatureDescription(Description = "This feature will add the '/twitter' widget to the current MVC Host.")]
-    public class TwitterWidgetMVCPlugin : IMVCPlugin
+    [Plugin(PluginType = typeof(IMvcPlugin))]
+    [MvcPluginDescription(Description = "This feature will add the '/twitter' widget to the current MVC Host.")]
+    public class TwitterWidgetMVCPlugin : IMvcPlugin
     {
         // Nothing to do here, just some feature discovery happening...
     }
