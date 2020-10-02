d5b17c3aaf90e0dd0032fd58a10aee4fbd768833
Maarten Merken <merken.maarten@gmail.com>
2020-10-02 16:36:27 +0200

    Added Plugin Testing lib

---
+++ src/Prise.Testing/Testing.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Prise
+{
+    public static class Testing
+    {
+        public static T CreateTestPluginInstance<T>(params object[] pluginServices)
+        {
+            var pluginType = typeof(T);
+            var pluginInstance = typeof(T).Assembly.CreateInstance(typeof(T).FullName);
+            var services = pluginType.GetTypeInfo().DeclaredFields.Where(f => f.CustomAttributes.Any(c => c.AttributeType.Name == typeof(Prise.Plugin.PluginServiceAttribute).Name));
+            foreach (var service in services)
+            {
+                var serviceType = service.FieldType;
+                var pluginService = pluginServices.FirstOrDefault(p => p.GetType().Name == serviceType.Name);
+                if (pluginService == null)
+                    throw new ArgumentException($"A pluginService of type {serviceType.Name} is required for activating plugin {pluginType.Name}.");
+
+                pluginInstance
+                    .GetType()
+                    .GetTypeInfo()
+                        .DeclaredFields
+                            .First(f => f.Name == service.Name)
+                            .SetValue(pluginInstance, pluginService);
+
+                var activationMethod = pluginType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public).FirstOrDefault(m => m.CustomAttributes.Any(c => c.AttributeType.Name == typeof(Prise.Plugin.PluginActivatedAttribute).Name));
+                activationMethod.Invoke(pluginInstance, null);
+            }
+
+            return (T)pluginInstance;
+        }
+    }
+}
